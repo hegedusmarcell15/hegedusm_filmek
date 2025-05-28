fbe158f31be40466f1bc534b910fc3776439bab4
hegedusmarcell15 <128583323+hegedusmarcell15@users.noreply.github.com>
2025-05-28 09:37:39 +0200

    Add files via upload

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using MySql.Data.MySqlClient;
 
 namespace hegedusm_filmek
 {
@@ -23,6 +24,58 @@ namespace hegedusm_filmek
         public MainWindow()
         {
             InitializeComponent();
+        }
+        string connStr = "server=localhost;user id=root;password=;database=hegedusm;charset=utf8";
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            lbAdatok.Items.Clear();
+            using (MySqlConnection conn = new MySqlConnection(connStr))
+            {
+                conn.Open();
+                string sql = "SELECT * FROM filmek";
+                MySqlCommand cmd = new MySqlCommand(sql, conn);
+                MySqlDataReader reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    string sor = $"{reader["filmazon"]};{reader["cim"]};{reader["ev"]};{reader["szines"]};{reader["mufaj"]};{reader["hossz"]}";
+                    lbAdatok.Items.Add(sor);
+                }
+                conn.Close();
+            }
+        }
+        private void LbAdatok_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (lbAdatok.SelectedItem != null)
+            {
+                string[] adatok = lbAdatok.SelectedItem.ToString().Split(';');
+                lbFilmAzon.Content = adatok[0];
+                tb1.Text = adatok[1];
+                tb2.Text = adatok[2];
+                tb3.Text = adatok[3];
+                tb4.Text = adatok[4];
+                tb5.Text = adatok[5];
+            }
+        }
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            using (MySqlConnection conn = new MySqlConnection(connStr))
+            {
+                conn.Open();
+                string sql = $@"
+            UPDATE filmek SET
+                cim = '{tb2.Text}',
+                ev = {tb3.Text},
+                szines = '{tb4.Text}',
+                mufaj = '{tb5.Text}',
+                hossz = {tb5.Text}
+            WHERE filmazon = '{lbFilmAzon.Content}'";
+                MySqlCommand cmd = new MySqlCommand(sql, conn);
+                cmd.ExecuteNonQuery();
+                conn.Close();
+                MessageBox.Show("Sikeres módosítás!");
+                Button_Click(null, null);
+            }
         }
     }
 }
